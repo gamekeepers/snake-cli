cd6a8f5c4c1090203fc2910f276a9daef74d3d0d
Vishva-lakkad07 <vishvalakkad07@gmail.com>
2025-09-17 18:57:42 +0530

    Fix: Updated scoring system 10 points per food item

--- snake.h
+++ snake.h
@@ -4,7 +4,7 @@
 #include <thread>
 #include <stdlib.h>
 #include <termios.h>
-#include <unistd.h> // for system clear
+#include <unistd.h>
 #include <map>
 #include <deque>
 #include <algorithm>
@@ -15,23 +15,19 @@ char direction='r';
 
 
 void input_handler(){
-    // change terminal settings
     struct termios oldt, newt;
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
-    // turn off canonical mode and echo
     newt.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
     map<char, char> keymap = {{'d', 'r'}, {'a', 'l'}, {'w', 'u'}, {'s', 'd'}, {'q', 'q'}};
     while (true) {
         char input = getchar();
         if (keymap.find(input) != keymap.end()) {
-            // This now correctly modifies the single, shared 'direction' variable
             direction = keymap[input];
         }else if (input == 'q'){
             exit(0);
         }
-        // You could add an exit condition here, e.g., if (input == 'q') break;
     }
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
@@ -52,6 +48,9 @@ void render_game(int size, deque<pair<int, int>> &snake, pair<int, int> food){
 }
 }
 
+
+
+
 pair<int,int> get_next_head(pair<int,int> current, char direction){
     pair<int, int> next; 
     if(direction =='r'){
@@ -68,34 +67,54 @@ pair<int,int> get_next_head(pair<int,int> current, char direction){
     
 }
 
-
-
-void game_play(){
+pair<int,int> generate_food(int size, const deque<pair<int,int>> &snake) {
+    pair<int,int> food;
+    do {
+        food = make_pair(rand() % size, rand() % size);
+    } while (find(snake.begin(), snake.end(), food) != snake.end());
+    return food;
+}
+void game_play() {
     system("clear");
     deque<pair<int, int>> snake;
-    snake.push_back(make_pair(0,0));
+    snake.push_back(make_pair(0, 0));
 
-    pair<int, int> food = make_pair(rand() % 10, rand() % 10);
-    for(pair<int, int> head=make_pair(0,1);; head = get_next_head(head, direction)){
-        // send the cursor to the top
+    pair<int, int> food = generate_food(10, snake);
+
+
+    int food_eaten = 0;
+    int speed = 500;
+    int score = 0;
+
+    for (pair<int, int> head = make_pair(0, 1);; head = get_next_head(head, direction)) {
         cout << "\033[H";
-        // check self collision
+
         if (find(snake.begin(), snake.end(), head) != snake.end()) {
             system("clear");
             cout << "Game Over" << endl;
             exit(0);
-        }else if (head.first == food.first && head.second == food.second) {
-            // grow snake
-            food = make_pair(rand() % 10, rand() % 10);
-            snake.push_back(head);            
-        }else{
-            // move snake
+        } else if (head.first == food.first && head.second == food.second) {
+            food = generate_food(10, snake);
+
+            snake.push_back(head);
+
+            food_eaten++;
+            score+=10;
+
+            if (food_eaten % 10 == 0 && speed > 100) {
+                speed += 50;
+            }
+        } else {
             snake.push_back(head);
             snake.pop_front();
         }
+
         render_game(10, snake, food);
         cout << "length of snake: " << snake.size() << endl;
-    
-        sleep_for(500ms);
+        cout << "Score: " << score << endl;
+
+        cout << "food eaten: " << food_eaten << " | current speed: " << speed << "ms" << endl;
+
+        sleep_for(std::chrono::milliseconds(speed));
     }
 }
