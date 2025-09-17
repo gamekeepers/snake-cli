085ff0bb4cdee76eb993b81ca72768e09a30f165
harsh katharotiya <harshkathrotiya2759@gmail.com>
2025-09-17 17:15:09 +0530

    Fix #6: Added Top 10 High Scores tracking and display

--- snake.h
+++ snake.h
@@ -13,6 +13,7 @@ using std::chrono::system_clock;
 using namespace std::this_thread;
 
 char direction='r';
+vector<int> high_scores;
 
 void input_handler(){
     // change terminal settings
@@ -65,6 +66,19 @@ pair<int,int> get_next_head(pair<int,int> current, char direction){
     return next;
 }
 
+void update_and_show_scores(int score) {
+    high_scores.push_back(score);
+    sort(high_scores.begin(), high_scores.end(), greater<int>());
+    if (high_scores.size() > 10) {
+        high_scores.resize(10);
+    }
+    cout << "\n=== Top 10 High Scores ===" << endl;
+    for (size_t i = 0; i < high_scores.size(); i++) {
+        cout << i + 1 << ". " << high_scores[i] << endl;
+    }
+    cout << "===========================" << endl;
+}
+
 void game_play() {
     system("clear");
     deque<pair<int, int>> snake;
@@ -85,6 +99,7 @@ void game_play() {
             system("clear");
             cout << "Game Over - You ran into yourself!" << endl;
             cout << "Final Score: " << score << endl;
+             update_and_show_scores(score);
             exit(0);
         } 
         // check poison collision
@@ -92,6 +107,7 @@ void game_play() {
             system("clear");
             cout << "Game Over - You ate poison!" << endl;
             cout << "Final Score: " << score << endl;
+            update_and_show_scores(score);
             exit(0);
         } 
         // check food collision
