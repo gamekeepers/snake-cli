e378a16312fb690e1055d8d1beb3ac4958e6dd17
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-09-17 18:09:13 +0530

    feat: Increase difficulty level as the game proceeds

--- snake.h
+++ snake.h
@@ -68,34 +68,46 @@ pair<int,int> get_next_head(pair<int,int> current, char direction){
     
 }
 
-
-
-void game_play(){
+void game_play() {
     system("clear");
     deque<pair<int, int>> snake;
-    snake.push_back(make_pair(0,0));
+    snake.push_back(make_pair(0, 0));
 
     pair<int, int> food = make_pair(rand() % 10, rand() % 10);
-    for(pair<int, int> head=make_pair(0,1);; head = get_next_head(head, direction)){
+
+    int food_eaten = 0;   // count how many food items eaten
+    int speed = 500;      // start speed in milliseconds
+
+    for (pair<int, int> head = make_pair(0, 1);; head = get_next_head(head, direction)) {
         // send the cursor to the top
         cout << "\033[H";
+
         // check self collision
         if (find(snake.begin(), snake.end(), head) != snake.end()) {
             system("clear");
             cout << "Game Over" << endl;
             exit(0);
-        }else if (head.first == food.first && head.second == food.second) {
+        } else if (head.first == food.first && head.second == food.second) {
             // grow snake
             food = make_pair(rand() % 10, rand() % 10);
-            snake.push_back(head);            
-        }else{
+            snake.push_back(head);
+
+            food_eaten++;
+
+           
+            if (food_eaten % 10 == 0 && speed > 100) {
+                speed += 50;  
+            }
+        } else {
             // move snake
             snake.push_back(head);
             snake.pop_front();
         }
+
         render_game(10, snake, food);
         cout << "length of snake: " << snake.size() << endl;
-    
-        sleep_for(500ms);
+        cout << "food eaten: " << food_eaten << " | current speed: " << speed << "ms" << endl;
+
+        sleep_for(std::chrono::milliseconds(speed));
     }
 }
