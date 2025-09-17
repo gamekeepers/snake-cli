5de02bed9c13f425adc8c8aa649b1c663277a4e4
MirajShekhda <mirjshekhada41@gmail.com>
2025-09-17 16:01:49 +0530

    Spawn poisonous food that snakes needs to avoid (Fixes #4)

--- snake.h
+++ snake.h
@@ -37,19 +37,21 @@ void input_handler(){
 }
 
 
-void render_game(int size, deque<pair<int, int>> &snake, pair<int, int> food){
+void render_game(int size, deque<pair<int, int>> &snake, pair<int, int> food, pair<int,int> poison){
     for(size_t i=0;i<size;i++){
         for(size_t j=0;j<size;j++){
             if (i == food.first && j == food.second){
                 cout << "🍎";
+            }else if (i == poison.first && j == poison.second){
+                cout << "💀";
             }else if (find(snake.begin(), snake.end(), make_pair(int(i), int(j))) != snake.end()) {
                 cout << "🐍";
             }else{
                 cout << "⬜";
             }
+        }
+        cout << endl;
     }
-    cout << endl;
-}
 }
 
 pair<int,int> get_next_head(pair<int,int> current, char direction){
@@ -76,36 +78,60 @@ void game_play() {
     snake.push_back(make_pair(0, 0));
 
     pair<int, int> food = make_pair(rand() % 10, rand() % 10);
+    pair<int, int> poison = make_pair(rand() % 10, rand() % 10);
+
     int food_count = 0;
-    int speed = 500;
-    int score = 0; // Score tracking
+    int speed = 500;   // initial speed in ms
+    int score = 0;     // score tracking
 
     for (pair<int, int> head = make_pair(0, 1); ; head = get_next_head(head, direction)) {
+        // send the cursor to the top of terminal
         cout << "\033[H";
 
+        // check self collision
         if (find(snake.begin(), snake.end(), head) != snake.end()) {
             system("clear");
-            cout << "Game Over" << endl;
+            cout << "Game Over - You ran into yourself!" << endl;
             cout << "Final Score: " << score << endl;
             exit(0);
-        } else if (head.first == food.first && head.second == food.second) {
+        } 
+        // check poison collision
+        else if (head.first == poison.first && head.second == poison.second) {
+            system("clear");
+            cout << "Game Over - You ate poison!" << endl;
+            cout << "Final Score: " << score << endl;
+            exit(0);
+        }
+        // check food collision
+        else if (head.first == food.first && head.second == food.second) {
             snake.push_back(head);
             food_count++;
-            score += 10; // Increment score per food
-            food = make_pair(rand() % 10, rand() % 10);
+            score += 10; // increment score per food
 
+            // spawn new food & poison
+            food = make_pair(rand() % 10, rand() % 10);
+            poison = make_pair(rand() % 10, rand() % 10);
+
+            // speed up every 10 foods (minimum 100ms)
             if (food_count % 10 == 0 && speed > 100) {
                 speed -= 50;
             }
-        } else {
+        } 
+        else {
+            // move snake forward
             snake.push_back(head);
             snake.pop_front();
         }
 
-        render_game(10, snake, food);
-        cout << "Length of snake: " << snake.size() << " | Score: " << score << endl;
+        // render board
+        render_game(10, snake, food, poison);
 
+        // show stats
+        cout << "Length of snake: " << snake.size() 
+             << " | Score: " << score 
+             << " | Speed: " << speed << "ms" << endl;
+
+        // control speed
         sleep_for(chrono::milliseconds(speed));
     }
 }
-
