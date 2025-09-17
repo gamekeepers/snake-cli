363e411498368228fbf608e9f1cd9c4195f2d9ab
vrushil123 <vrushilparikh123@gmail.com>
2025-09-17 13:52:12 +0530

    test cases generated

--- snake_test.cpp
+++ snake_test.cpp
@@ -25,7 +25,40 @@ TEST(SnakeBehaviour, NextHeadDown) {
   EXPECT_EQ(get_next_head(current, 'd'),make_pair(current.first+1,current.second));
   
 }
+// Food generation should not overlap snake or another food
+TEST(FoodGeneration, NotOnSnakeOrOtherFood) {
+    deque<pair<int,int>> snake = {{0,0}, {0,1}, {0,2}};
+    pair<int,int> other_food = {5,5};
+    for(int i=0;i<100;i++){ // test multiple generations
+        pair<int,int> food = generate_food(10, snake, other_food);
+        EXPECT_EQ(find(snake.begin(), snake.end(), food), snake.end());
+        EXPECT_NE(food, other_food);
+    }
+}
 
+// Calculate speed should never go below min speed
+TEST(SpeedCalculation, MinSpeedLimit) {
+    int speed = calculate_speed(20); // more than enough to reduce below min
+    EXPECT_GE(speed, 100);
+}
+
+// Score increments properly
+TEST(ScoreUpdate, IncrementBy10) {
+    int score = 0;
+    score += 10;
+    EXPECT_EQ(score, 10);
+}
+
+// Poison food should never overlap snake or normal food
+TEST(PoisonFood, NotOnSnakeOrFood) {
+    deque<pair<int,int>> snake = {{0,0}, {1,0}, {2,0}};
+    pair<int,int> normal_food = {5,5};
+    for(int i=0;i<50;i++){
+        pair<int,int> poison = generate_food(10, snake, normal_food);
+        EXPECT_EQ(find(snake.begin(), snake.end(), poison), snake.end());
+        EXPECT_NE(poison, normal_food);
+    }
+}
 
 /** 
  * g++ -o my_tests snake_test.cpp -lgtest -lgtest_main -pthread;
