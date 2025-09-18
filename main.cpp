9bc0a10c67cd74a7d66cf323adea579f23c9ea57
kushali13 <kushalisanghvi1311@gmail.com>
2025-09-18 13:56:23 +0530

    issues solved. all 6 of them.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "snake.h"
+#include <ctime>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    srand((unsigned)time(nullptr));
+
     thread input_thread(input_handler);
-    thread game_thread(game_play);   
+    thread game_thread(game_play);
+
     input_thread.join();
     game_thread.join();
-return 0;
+    return 0;
 }
