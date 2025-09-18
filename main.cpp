e83ad01602d384fa7edd4b25286753d855c3badf
Kalyani Dave <kalyanidave2004@gmail.com>
2025-09-18 15:26:39 +0530

    Implement all 6 requested features: scoring, difficulty, poison food, pause, high scores, smart spawning

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include "snake.h"
 
 int main(int argc, char *argv[]) {
-    thread input_thread(input_handler);
-    thread game_thread(game_play);   
-    input_thread.join();
-    game_thread.join();
-return 0;
+    game_play();
+    return 0;
 }
