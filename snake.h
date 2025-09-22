d24c774108d040eef3682ab27a39ffa0d392fce3
manan.pandya <manan.pandya@globallogic.com>
2025-09-22 14:18:24 +0530

    Added all 6 enhancements and then added encapsulation wherever possible with a new refactored main method

--- snake.h
+++ snake.h
@@ -4,98 +4,172 @@
 #include <thread>
 #include <stdlib.h>
 #include <termios.h>
-#include <unistd.h> // for system clear
+#include <unistd.h>
 #include <map>
 #include <deque>
 #include <algorithm>
 using namespace std;
-using std::chrono::system_clock;
 using namespace std::this_thread;
-char direction='r';
 
+class HighScores {
+    vector<int> scores;
+public:
+    void update(int score) {
+        scores.push_back(score);
+        sort(scores.begin(), scores.end(), greater<int>());
+        if (scores.size() > 10) scores.resize(10);
+    }
+    void show() const {
+        cout << "\n=== TOP 10 HIGH SCORES ===\n";
+        if (scores.empty()) {
+            cout << "No scores yet.\n";
+            return;
+        }
+        for (size_t i = 0; i < scores.size(); i++) {
+            cout << i + 1 << ". " << scores[i] << endl;
+        }
+    }
+};
 
-void input_handler(){
-    // change terminal settings
+class Game {
+    int size;
+    deque<pair<int, int>> snake;
+    pair<int, int> food;
+    pair<int, int> poison;
+    char direction;
+    bool paused;
+    int score;
+    int steps;
+    int foodsEaten;
+    int speedMs;
+    HighScores &highScores; // reference to shared highscore tracker
+
+    pair<int, int> randomCellExcluding(const pair<int, int> &other = {-1, -1}) {
+        pair<int, int> p;
+        do {
+            p = make_pair(rand() % size, rand() % size);
+        } while (find(snake.begin(), snake.end(), p) != snake.end() || p == other);
+        return p;
+    }
+
+    pair<int, int> getNextHead(pair<int, int> current) {
+        if (direction == 'r') {
+            return make_pair(current.first, (current.second + 1) % size);
+        } else if (direction == 'l') {
+            return make_pair(current.first, current.second == 0 ? size - 1 : current.second - 1);
+        } else if (direction == 'd') {
+            return make_pair((current.first + 1) % size, current.second);
+        } else if (direction == 'u') {
+            return make_pair(current.first == 0 ? size - 1 : current.first - 1, current.second);
+        }
+        return current;
+    }
+
+    void render() {
+        for (size_t i = 0; i < size; i++) {
+            for (size_t j = 0; j < size; j++) {
+                if (i == food.first && j == food.second) {
+                    cout << "🍎";
+                } else if (i == poison.first && j == poison.second) {
+                    cout << "💀";
+                } else if (find(snake.begin(), snake.end(), make_pair(int(i), int(j))) != snake.end()) {
+                    cout << "🐍";
+                } else {
+                    cout << "⬜";
+                }
+            }
+            cout << endl;
+        }
+        cout << "Length: " << snake.size()
+             << " | Score: " << score
+             << (paused ? " | PAUSED (press 'p' to resume)" : "") << endl;
+    }
+
+    void gameOver(const string &reason) {
+        system("clear");
+        cout << "Game Over - " << reason << "\nFinal Score: " << score << endl;
+        highScores.update(score);
+        highScores.show();
+        exit(0);
+    }
+
+public:
+    Game(int gridSize, HighScores &hs)
+        : size(gridSize), direction('r'), paused(false), score(0), steps(0),
+          foodsEaten(0), speedMs(500), highScores(hs) {
+        snake.push_back(make_pair(0, 0));
+        food = randomCellExcluding();
+        poison = randomCellExcluding(food);
+    }
+
+    void togglePause() { paused = !paused; }
+
+    void changeDirection(char newDir) { direction = newDir; }
+
+    //this is the loop wherein snake goes about doing its job
+    void loop() {
+        for (pair<int, int> head = make_pair(0, 1);; head = getNextHead(head, direction)) {
+            cout << "\033[H";
+            if (paused) {
+                render();
+                sleep_for(200ms);
+                continue;
+            }
+
+            // self collision - feature
+            if (find(snake.begin(), snake.end(), head) != snake.end()) {
+                gameOver("You hit yourself!");
+            }
+
+            // poison collision - feature
+            if (head == poison) {
+                gameOver("You ate poison!");
+            }
+ 
+            // normal food  - feature
+            if (head == food) {
+                snake.push_back(head);
+                score += 10;
+                foodsEaten++;
+                if (foodsEaten % 10 == 0 && speedMs > 100) speedMs -= 50;
+                food = randomCellExcluding(poison);
+            } else {
+                snake.push_back(head);
+                snake.pop_front();
+            }
+
+            // move poison - feature
+            steps++;
+            if (steps % 10 == 0) poison = randomCellExcluding(food);
+
+            render();
+            sleep_for(std::chrono::milliseconds(speedMs));
+        }
+    }
+};
+
+// Separate input handler function but pass Game to it -> this is to pass the game to it and separate input handler, to make it easy
+void inputHandler(Game &game) {
     struct termios oldt, newt;
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
-    // turn off canonical mode and echo
     newt.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    map<char, char> keymap = {{'d', 'r'}, {'a', 'l'}, {'w', 'u'}, {'s', 'd'}, {'q', 'q'}};
+
+    map<char, char> keymap = {
+        {'d', 'r'}, {'a', 'l'}, {'w', 'u'}, {'s', 'd'}
+    };
     while (true) {
         char input = getchar();
         if (keymap.find(input) != keymap.end()) {
-            // This now correctly modifies the single, shared 'direction' variable
-            direction = keymap[input];
-        }else if (input == 'q'){
+            game.changeDirection(keymap[input]);
+        } else if (input == 'q') {
             exit(0);
+        } else if (input == 'p') {
+            game.togglePause();
         }
-        // You could add an exit condition here, e.g., if (input == 'q') break;
     }
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
 
 
-void render_game(int size, deque<pair<int, int>> &snake, pair<int, int> food){
-    for(size_t i=0;i<size;i++){
-        for(size_t j=0;j<size;j++){
-            if (i == food.first && j == food.second){
-                cout << "🍎";
-            }else if (find(snake.begin(), snake.end(), make_pair(int(i), int(j))) != snake.end()) {
-                cout << "🐍";
-            }else{
-                cout << "⬜";
-            }
-    }
-    cout << endl;
-}
-}
-
-pair<int,int> get_next_head(pair<int,int> current, char direction){
-    pair<int, int> next; 
-    if(direction =='r'){
-        next = make_pair(current.first,(current.second+1) % 10);
-    }else if (direction=='l')
-    {
-        next = make_pair(current.first, current.second==0?9:current.second-1);
-    }else if(direction =='d'){
-            next = make_pair((current.first+1)%10,current.second);
-        }else if (direction=='u'){
-            next = make_pair(current.first==0?9:current.first-1, current.second);
-        }
-    return next;
-    
-}
-
-
-
-void game_play(){
-    system("clear");
-    deque<pair<int, int>> snake;
-    snake.push_back(make_pair(0,0));
-
-    pair<int, int> food = make_pair(rand() % 10, rand() % 10);
-    for(pair<int, int> head=make_pair(0,1);; head = get_next_head(head, direction)){
-        // send the cursor to the top
-        cout << "\033[H";
-        // check self collision
-        if (find(snake.begin(), snake.end(), head) != snake.end()) {
-            system("clear");
-            cout << "Game Over" << endl;
-            exit(0);
-        }else if (head.first == food.first && head.second == food.second) {
-            // grow snake
-            food = make_pair(rand() % 10, rand() % 10);
-            snake.push_back(head);            
-        }else{
-            // move snake
-            snake.push_back(head);
-            snake.pop_front();
-        }
-        render_game(10, snake, food);
-        cout << "length of snake: " << snake.size() << endl;
-    
-        sleep_for(500ms);
-    }
-}
